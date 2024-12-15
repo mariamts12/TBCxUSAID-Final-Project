ecefc4fdc6d4205a657f671dbda1d416ef37490d
mariamts <matsut21@freeuni.edu.ge>
2024-12-15 21:40:34 +0400

    create project

---
+++ magic_loop/magic_loop/swagger.py
@@ -0,0 +1,15 @@
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Magic Loop App API",
+        default_version="v1",
+        description="Magic Loop App API",
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+    authentication_classes=[JWTAuthentication],
+)
